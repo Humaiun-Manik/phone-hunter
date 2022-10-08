c7b986dc7c60c72d68324f3e2f2c7a50825b1374
humaiun <humaiunkobirr@gmail.com>
2022-10-09 02:41:52 +0600

    add search box

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+});
